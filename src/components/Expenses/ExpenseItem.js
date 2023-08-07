0f9245390bfe66bfb97384d705ff030c015e4520
Andy Gorbould <gorbould@mail.com>
2023-08-07 18:33:00 +0200

    added button to ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,6 +10,7 @@ const ExpenseItem = (props) => {          //changed to arrow function
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button>Change Title</button>
     </Card>
   );
 }
