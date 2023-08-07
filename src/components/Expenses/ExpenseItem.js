8a31ad7719eb0dac19ed5ac4d10bc3397618a897
Andy Gorbould <gorbould@mail.com>
2023-08-07 18:48:32 +0200

    added clickHandler to ExpenseItem button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {          //changed to arrow function
+  const clickHandler = () => {
+    console.log('Clicked')
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,7 +14,7 @@ const ExpenseItem = (props) => {          //changed to arrow function
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
