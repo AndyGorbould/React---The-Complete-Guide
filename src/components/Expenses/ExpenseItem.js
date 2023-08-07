462b7931b1eb0e3c11b5ae999d296e8345d3ae79
Andy Gorbould <gorbould@mail.com>
2023-08-07 19:14:18 +0200

    added useState to button's clickHandler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {          //changed to arrow function
-  const clickHandler = () => {
-    console.log('Clicked')
+
+  const [title, setTitle] = useState(props.title);           // useState: call inside component functions
+
+  const clickHandler = () => {            // xxxHandler is the typical way of naming event functions
+    setTitle("Updated");            // component is automatically re-rendered on state change
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
