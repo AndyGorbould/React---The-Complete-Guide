3546afb92eb8f343d10aa83e66590eaddfb62c0e
Andy Gorbould <gorbould@mail.com>
2023-08-08 17:29:51 +0200

    added saveExpenseHandler to NewExpense & called it in submitHandler in ExpenseForm to receive expenseData object

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,18 @@ import ExpenseForm from "./ExpenseForm.js";
 import './NewExpense.css';
 
 const NewExpense = () => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()              // could be replaced with GUID or UUID to stop duplicate possibilities
+        };
+        console.log(expenseData);
+    };
+
     return (
     <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
     );
 };
