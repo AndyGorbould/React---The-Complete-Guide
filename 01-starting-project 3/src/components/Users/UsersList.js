2c94dc3e0104c7e27d36f8916c8aa7e1c9c808b0
Andy Gorbould <gorbould@mail.com>
2023-08-21 22:32:50 +0200

    added css module for UsersList component

--- 01-starting-project 3/src/components/Users/UsersList.js
+++ 01-starting-project 3/src/components/Users/UsersList.js
@@ -1,17 +1,18 @@
 import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
   return (
-    <Card>
-        <ul>
-            {props.users.map((user => (
-                <li>
-                    {user.name} ({user.age} years old)
-                </li>
-            )))}
-        </ul>
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
     </Card>
-    );
+  );
 };
 
-export default UsersList
+export default UsersList;
