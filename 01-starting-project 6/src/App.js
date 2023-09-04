ca4ea6771fe9c798ae79d659f9db08a6e235ffa9
Andy Gorbould <gorbould@mail.com>
2023-09-04 11:43:25 +0200

    inline IF on the Cart in App. showCartHandler function passed down to Header & HeaderCartButton components

--- 01-starting-project 6/src/App.js
+++ 01-starting-project 6/src/App.js
@@ -16,8 +16,8 @@ function App() {
 
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
