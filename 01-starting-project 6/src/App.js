0eb23b9c52557154f77d64480857339333f8a1c9
Andy Gorbould <gorbould@mail.com>
2023-09-04 11:34:40 +0200

    added cartIsShown state to App

--- 01-starting-project 6/src/App.js
+++ 01-starting-project 6/src/App.js
@@ -1,9 +1,11 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
   return (
     <Fragment>
       <Cart />
