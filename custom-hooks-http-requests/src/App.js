d80a2f4725885b3f77ea468f204cfd3ca9b3bb88
Andy Gorbould <gorbould@mail.com>
2023-09-13 14:59:15 +0200

    refactored App & useHttp to avoid dependencies

--- custom-hooks-http-requests/src/App.js
+++ custom-hooks-http-requests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -7,25 +7,25 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = useCallback((tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  }, []); // no external depenencies
-
   // (requestConfig, applyData) // destructuring directly
   const {
     isLoading,
     error,
     sendRequest: fetchTasks, // alias - 'sendRequest recieved, now named 'fetchTasks'
-  } = useHttp(transformTasks);
+  } = useHttp();
 
   useEffect(() => {
-    fetchTasks({ url: process.env.REACT_APP_FIREBASE_URL });
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks({ url: process.env.REACT_APP_FIREBASE_URL }, transformTasks);
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
