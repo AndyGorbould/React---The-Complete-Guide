02d11242a656f0448c7c3cf5c24568f31d8d2d8b
Andy Gorbould <gorbould@mail.com>
2023-09-18 11:12:35 +0200

    error handled inside useEffect, error simulated with URL spelling mistake

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -6,10 +6,17 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch(process.env.REACT_APP_FIREBASE_URL);
+      // const response = await fetch(process.env.REACT_APP_FIREBASE_URL);
+      const response = await fetch(process.env.REACT_APP_FIREBASE_URLL); // to simulate error
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       console.log(responseData);
@@ -28,13 +35,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []); // no dependencies, so only runs when first loaded, which is ideal for this application!
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading....</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
